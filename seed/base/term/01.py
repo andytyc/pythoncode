84217ea4d64d2a5780ca9be20ddf7555b85ba922
andytyc <andy.tengyc@gmail.com>
2023-02-03 18:19:44 +0800

    updated dir

--- seed/base/term/01.py
+++ seed/base/term/01.py
@@ -1,11 +1,14 @@
 x = 2
 print(type(x))
-# <class 'int'>
 
 x = 2.3
 print(type(x))
-# <class 'float'>
 
 x = [2, 3]
 print(type(x))
-# <class 'list'>
+
+'''
+<class 'int'>
+<class 'float'>
+<class 'list'>
+'''
---
+++ seed/base/term/02.py
@@ -0,0 +1,15 @@
+print(issubclass(bool, int))
+
+print(True == 1)
+print(False == 0)
+
+print(True is 1)
+print(False is 0)
+
+'''
+True
+True
+True
+False
+False
+'''
